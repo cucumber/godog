aa08819bb30257a4c829a9174298776e58a7f67c
gedi <gediminas.morkevicius@gmail.com>
2015-06-10 15:02:30 +0300

    register steps like http handler

---
+++ steps.go
@@ -0,0 +1,23 @@
+package behat
+
+import "regexp"
+
+var stepHandlers map[*regexp.Regexp]StepHandler
+
+type StepHandler interface {
+	HandleStep(args ...interface{}) error
+}
+
+type StepHandlerFunc func(...interface{}) error
+
+func (f StepHandlerFunc) HandleStep(args ...interface{}) error {
+	return f(args...)
+}
+
+func Step(exp *regexp.Regexp, h StepHandler) {
+	stepHandlers[exp] = h
+}
+
+func init() {
+	stepHandlers = make(map[*regexp.Regexp]StepHandler)
+}
