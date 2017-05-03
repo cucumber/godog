2b109b5ea857b9be592afa587bcaed0ee14bcb6d
Matthew Rothenberg <mrothenberg@gmail.com>
2017-05-03 17:52:50 -0400

    move SuiteContext out of `_test` namespace
    This allows it to be easily re-used in other libraries to do
    meta-testing on their own godog features/steps.
    
    At some point it may make sense to migrate this entirely to a
    sub-package of godog, however that would require a larger
    re-architecting of the project structure to avoid circular dependencies
    (e.g. `import cycle not allowed`).

--- fmt_test.go
+++ fmt_test.go
@@ -1,38 +1,6 @@
 package godog
 
-import (
-	"io"
-	"testing"
-
-	"github.com/DATA-DOG/godog/gherkin"
-)
-
-type testFormatter struct {
-	basefmt
-	scenarios []interface{}
-}
-
-func testFormatterFunc(suite string, out io.Writer) Formatter {
-	return &testFormatter{
-		basefmt: basefmt{
-			started: timeNowFunc(),
-			indent:  2,
-			out:     out,
-		},
-	}
-}
-
-func (f *testFormatter) Node(node interface{}) {
-	f.basefmt.Node(node)
-	switch t := node.(type) {
-	case *gherkin.Scenario:
-		f.scenarios = append(f.scenarios, t)
-	case *gherkin.ScenarioOutline:
-		f.scenarios = append(f.scenarios, t)
-	}
-}
-
-func (f *testFormatter) Summary() {}
+import "testing"
 
 func TestShouldFindFormatter(t *testing.T) {
 	cases := map[string]bool{
