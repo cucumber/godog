3521cd2483fa8fe4082d8f5dda292f706d6c3262
Rickard Englund <rickard.englund@skf.com>
2020-12-16 13:05:32 +0100

    add test case for duplicate pickle ids

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,6 +1,10 @@
 package parser_test
 
 import (
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -30,5 +34,46 @@ func Test_FeatureFilePathParser(t *testing.T) {
 		p, ln := parser.ExtractFeaturePathLine(c.input)
 		assert.Equal(t, p, c.path)
 		assert.Equal(t, ln, c.line)
+	}
+}
+
+func Test_ParseFeatures_FromMultiplePaths(t *testing.T) {
+	const featureFileName = "godogs.feature"
+	const featureFileContents = `Feature: eat godogs
+  In order to be happy
+  As a hungry gopher
+  I need to be able to eat godogs
+
+  Scenario: Eat 5 out of 12
+    Given there are 12 godogs
+    When I eat 5
+		Then there should be 7 remaining`
+
+	baseDir := filepath.Join(os.TempDir(), t.Name(), "godogs")
+	errA := os.MkdirAll(baseDir+"/a", 0755)
+	errB := os.MkdirAll(baseDir+"/b", 0755)
+	defer os.RemoveAll(baseDir)
+
+	require.Nil(t, errA)
+	require.Nil(t, errB)
+
+	err := ioutil.WriteFile(filepath.Join(baseDir+"/a", featureFileName), []byte(featureFileContents), 0644)
+	require.Nil(t, err)
+	err = ioutil.WriteFile(filepath.Join(baseDir+"/b", featureFileName), []byte(featureFileContents), 0644)
+	require.Nil(t, err)
+
+	features, err := parser.ParseFeatures("", []string{baseDir + "/a", baseDir + "/b"})
+	assert.Nil(t, err)
+	assert.Len(t, features, 2)
+
+	pickleIDs := map[string]bool{}
+	for _, f := range features {
+		for _, p := range f.Pickles {
+			if pickleIDs[p.Id] {
+				assert.Failf(t, "found duplicate pickle ID", "Pickle ID %s was already used", p.Id)
+			}
+
+			pickleIDs[p.Id] = true
+		}
 	}
 }
