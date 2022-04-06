17e72cf642591ea2c5411b1661f591ad49a66426
Arianna Cooper <aricooper55@gmail.com>
2022-04-06 12:32:32 -0700

    undid debugging changes

--- cmd/godog/internal/cmd_build.go
+++ cmd/godog/internal/cmd_build.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"fmt"
 	"go/build"
+	"path/filepath"
 
 	"github.com/cucumber/godog/internal/builder"
 	"github.com/spf13/cobra"
@@ -36,12 +37,10 @@ The test runner can be executed with the same flags as when using godog run.`,
 }
 
 func buildCmdRunFunc(cmd *cobra.Command, args []string) error {
-	//bin, err := filepath.Abs(buildOutput)
-	bin := "/home/arianna/godog/godog.test"
-	var err error
-	//if err != nil {
-	//	return fmt.Errorf("could not locate absolute path for: %q. reason: %v", buildOutput, err)
-	//}
+	bin, err := filepath.Abs(buildOutput)
+	if err != nil {
+		return fmt.Errorf("could not locate absolute path for: %q. reason: %v", buildOutput, err)
+	}
 
 	if err = builder.Build(bin); err != nil {
 		return fmt.Errorf("could not build binary at: %q. reason: %v", buildOutput, err)
