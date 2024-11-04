1d83af8ebfb299b66221bc174270b8ad68eb5930
Johnlon <836248+Johnlon@users.noreply.github.com>
2024-11-04 03:51:46 +0000

    rationalised the suite.go runSteps function.

--- internal/testutils/utils.go
+++ internal/testutils/utils.go
@@ -1,6 +1,9 @@
 package testutils
 
 import (
+	"bytes"
+	"fmt"
+	perr "github.com/pkg/errors"
 	"strings"
 	"testing"
 
@@ -108,3 +111,105 @@ Scenario Outline: Eat <dec> out of <beginning>
   Examples:
 	| begin | dec | remain |
 	| 12    | 5   | 7      |`
+
+func VDiffString(expected, actual string) {
+	list1 := strings.Split(expected, "\n")
+	list2 := strings.Split(actual, "\n")
+
+	VDiffLists(list1, list2)
+}
+
+func VDiffLists(list1 []string, list2 []string) error {
+
+	var diffs bool
+	buf := &bytes.Buffer{}
+
+	// wrapString wraps a string into chunks of the given width.
+	wrapString := func(str string, width int) []string {
+		return []string{str}
+	}
+	// Get the length of the longer list
+	maxLength := len(list1)
+	if len(list2) > maxLength {
+		maxLength = len(list2)
+	}
+	maxlen := 0
+	for i := 0; i < len(list1); i++ {
+		if len(list1[i]) > maxlen {
+			maxlen = len(list1[i])
+		}
+	}
+	for i := 0; i < len(list2); i++ {
+		if len(list2[i]) > maxlen {
+			maxlen = len(list2[i])
+		}
+	}
+
+	colWid := maxlen
+	fmtTitle := fmt.Sprintf("%2s %%3s: %%-%ds | %%-%ds\n", "", colWid+2, colWid+2)
+	fmtData := fmt.Sprintf("%%2s %%3d: %%-%ds | %%-%ds\n", colWid+2, colWid+2)
+
+	fmt.Fprintf(buf, fmtTitle, "#", "expected", "actual")
+
+	for i := 0; i < maxLength; i++ {
+		var val1, val2 string
+
+		// Get the value from list1 if it exists
+		if i < len(list1) {
+			val1 = list1[i]
+		} else {
+			val1 = "N/A"
+		}
+
+		// Get the value from list2 if it exists
+		if i < len(list2) {
+			val2 = list2[i]
+		} else {
+			val2 = "N/A"
+		}
+
+		// Wrap both strings into slices of strings with fixed width
+		wrapped1 := wrapString(val1, colWid)
+		wrapped2 := wrapString(val2, colWid)
+
+		// Find the number of wrapped lines needed for the current pair
+		maxWrappedLines := len(wrapped1)
+		if len(wrapped2) > maxWrappedLines {
+			maxWrappedLines = len(wrapped2)
+		}
+
+		// Print the wrapped lines with alignment
+		for j := 0; j < maxWrappedLines; j++ {
+			var line1, line2 string
+
+			// Get the wrapped line or use an empty string if it doesn't exist
+			if j < len(wrapped1) {
+				line1 = wrapped1[j]
+			} else {
+				line1 = ""
+			}
+
+			if j < len(wrapped2) {
+				line2 = wrapped2[j]
+			} else {
+				line2 = ""
+			}
+
+			status := "  "
+			// if val1 != val2 {
+			if strings.TrimSpace(line1) != strings.TrimSpace(line2) {
+				status = "* "
+				diffs = true
+			}
+
+			delim := "¬"
+			// Print the wrapped lines with fixed-width column
+			fmt.Fprintf(buf, fmtData, status, i+1, delim+line1+delim, delim+line2+delim)
+		}
+	}
+
+	if diffs {
+		return perr.New(buf.String())
+	}
+	return nil
+}
--- suite_context_test.go
+++ suite_context_test.go
@@ -7,6 +7,7 @@ import (
 	"encoding/xml"
 	"errors"
 	"fmt"
+	"github.com/cucumber/godog/internal/testutils"
 	"path/filepath"
 	"regexp"
 	"sort"
@@ -384,7 +385,6 @@ func (tc *godogFeaturesScenario) theUndefinedStepSnippetsShouldBe(body *DocStrin
 	expected := tc.cleanupSnippet(body.Content)
 
 	if actual != expected {
-		fmt.Printf("actual:\n%s\nexpected:\n%s\n", actual, expected)
 		return fmt.Errorf("snippets do not match actual: %s", f.Snippets())
 	}
 
@@ -543,7 +543,7 @@ func (tc *godogFeaturesScenario) followingStepsShouldHave(status string, steps *
 	sorted(actual)
 	sorted(expected)
 
-	err := VDiffLists(expected, actual)
+	err := testutils.VDiffLists(expected, actual)
 	return err
 }
 
@@ -829,7 +829,7 @@ func (tc *godogFeaturesScenario) theRenderJSONWillBe(docstring *DocString) error
 
 	err := assertExpectedAndActual(assert.Equal, expected, actual)
 	if err != nil {
-		VDiffString(expectedString, actualString)
+		testutils.VDiffString(expectedString, actualString)
 	}
 	return err
 }
@@ -1249,106 +1249,4 @@ func TestTestSuite_Run(t *testing.T) {
 			assert.Equal(t, expectedLines, actualLines, log)
 		})
 	}
-}
-
-func VDiffString(expected, actual string) {
-	list1 := strings.Split(expected, "\n")
-	list2 := strings.Split(actual, "\n")
-
-	VDiffLists(list1, list2)
-}
-
-func VDiffLists(list1 []string, list2 []string) error {
-
-	var diffs bool
-	buf := &bytes.Buffer{}
-
-	// wrapString wraps a string into chunks of the given width.
-	wrapString := func(str string, width int) []string {
-		return []string{str}
-	}
-	// Get the length of the longer list
-	maxLength := len(list1)
-	if len(list2) > maxLength {
-		maxLength = len(list2)
-	}
-	maxlen := 0
-	for i := 0; i < len(list1); i++ {
-		if len(list1[i]) > maxlen {
-			maxlen = len(list1[i])
-		}
-	}
-	for i := 0; i < len(list2); i++ {
-		if len(list2[i]) > maxlen {
-			maxlen = len(list2[i])
-		}
-	}
-
-	colWid := maxlen
-	fmtTitle := fmt.Sprintf("%2s %%3s: %%-%ds | %%-%ds\n", "", colWid+2, colWid+2)
-	fmtData := fmt.Sprintf("%%2s %%3d: %%-%ds | %%-%ds\n", colWid+2, colWid+2)
-
-	fmt.Printf(fmtTitle, "#", "expected", "actual")
-
-	for i := 0; i < maxLength; i++ {
-		var val1, val2 string
-
-		// Get the value from list1 if it exists
-		if i < len(list1) {
-			val1 = list1[i]
-		} else {
-			val1 = "N/A"
-		}
-
-		// Get the value from list2 if it exists
-		if i < len(list2) {
-			val2 = list2[i]
-		} else {
-			val2 = "N/A"
-		}
-
-		// Wrap both strings into slices of strings with fixed width
-		wrapped1 := wrapString(val1, colWid)
-		wrapped2 := wrapString(val2, colWid)
-
-		// Find the number of wrapped lines needed for the current pair
-		maxWrappedLines := len(wrapped1)
-		if len(wrapped2) > maxWrappedLines {
-			maxWrappedLines = len(wrapped2)
-		}
-
-		// Print the wrapped lines with alignment
-		for j := 0; j < maxWrappedLines; j++ {
-			var line1, line2 string
-
-			// Get the wrapped line or use an empty string if it doesn't exist
-			if j < len(wrapped1) {
-				line1 = wrapped1[j]
-			} else {
-				line1 = ""
-			}
-
-			if j < len(wrapped2) {
-				line2 = wrapped2[j]
-			} else {
-				line2 = ""
-			}
-
-			status := "  "
-			// if val1 != val2 {
-			if strings.TrimSpace(line1) != strings.TrimSpace(line2) {
-				status = "* "
-				diffs = true
-			}
-
-			delim := "¬"
-			// Print the wrapped lines with fixed-width column
-			fmt.Fprintf(buf, fmtData, status, i+1, delim+line1+delim, delim+line2+delim)
-		}
-	}
-
-	if diffs {
-		return perr.New(buf.String())
-	}
-	return nil
 }
