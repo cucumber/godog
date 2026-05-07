e74e7a5e2c246a9c5eababe34c7f11988e9c6753
Kevin Cross <kcross@cleartoken.io>
2026-05-07 19:33:57 +0100

    fix(tags): the ',' operator was not working as an and operator in the tags as specified
    fix: go mod tidy examples
    
    fixup

--- internal/tags/tag_filter_test.go
+++ internal/tags/tag_filter_test.go
@@ -20,29 +20,30 @@ type testcase struct {
 var testdata = []*pickle{p1, p2, p3}
 var p1 = &pickle{Id: "one", Tags: []*tag{{Name: "@one"}, {Name: "@wip"}}}
 var p2 = &pickle{Id: "two", Tags: []*tag{{Name: "@two"}, {Name: "@wip"}}}
-var p3 = &pickle{Id: "three", Tags: []*tag{{Name: "@hree"}, {Name: "@wip"}}}
-
-var testcases = []testcase{
-	{filter: "", expected: testdata},
-
-	{filter: "@one", expected: []*pickle{p1}},
-	{filter: "~@one", expected: []*pickle{p2, p3}},
-	{filter: "one", expected: []*pickle{p1}},
-	{filter: " one ", expected: []*pickle{p1}},
-
-	{filter: "@one,@two", expected: []*pickle{p1, p2}},
-	{filter: "@one,~@two", expected: []*pickle{p1, p3}},
-	{filter: " @one , @two ", expected: []*pickle{p1, p2}},
-
-	{filter: "@one&&@two", expected: []*pickle{}},
-	{filter: "@one&&~@two", expected: []*pickle{p1}},
-	{filter: "@one&&@wip", expected: []*pickle{p1}},
-
-	{filter: "@one&&@two,@wip", expected: []*pickle{p1}},
-}
+var p3 = &pickle{Id: "three", Tags: []*tag{{Name: "@three"}, {Name: "@wip"}}}
 
 func Test_ApplyTagFilter(t *testing.T) {
-	for _, tc := range testcases {
+	for _, tc := range []testcase{
+		{filter: "", expected: testdata},
+
+		{filter: "@one", expected: []*pickle{p1}},
+		{filter: "~@one", expected: []*pickle{p2, p3}},
+		{filter: "one", expected: []*pickle{p1}},
+		{filter: " one ", expected: []*pickle{p1}},
+		//5
+		{filter: "@one,@two", expected: []*pickle{p1, p2}},
+		{filter: "@one,~@two", expected: []*pickle{p1, p3}},
+		{filter: " @one , @two ", expected: []*pickle{p1, p2}},
+
+		{filter: "@one&&@two", expected: []*pickle{}},
+		{filter: "@one&&~@two", expected: []*pickle{p1}},
+		{filter: "@one&&@wip", expected: []*pickle{p1}},
+
+		{filter: "@one&&@two,@wip", expected: []*pickle{p1, p2, p3}},
+		{filter: "@one&&@wip,@wip&&@two", expected: []*pickle{p1, p2}},
+		{filter: "@one&&@two,@one&&@wip,@wip&&@two", expected: []*pickle{p1, p2}},
+		{filter: "@two&&@one,@one&&@wip,@three&&@two", expected: []*pickle{p1}},
+	} {
 		t.Run("", func(t *testing.T) {
 			actual := tags.ApplyTagFilter(tc.filter, testdata)
 			assert.Equal(t, tc.expected, actual)
